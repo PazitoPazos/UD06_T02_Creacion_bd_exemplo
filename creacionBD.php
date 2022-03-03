58df6178b71281b55080a6c8a792451ab4c1ef7f
PazitoPazos <javier.pazos001@gmail.com>
2022-03-03 13:07:49 +0100

    Añadido creacionBD.php

---
+++ creacionBD.php
@@ -0,0 +1,24 @@
+<?php
+	//Conexión
+	$conexionBD = @mysqli_connect('localhost', 'Pazos', '1234');
+
+	if (!$conexionBD) {
+		echo('Erro número ' . mysqli_connect_errno() . ' ao establecer a conexión: ' . mysqli_connect_error() . '.<br/>');
+	} else {
+		echo('Conexión establecida con éxito<br/>');
+
+		//Creación dunha BD
+		if (mysqli_query($conexionBD, 'CREATE DATABASE iaw2') === TRUE) {
+		echo('Creouse correctamente a BD chamada iaw2.<br/>');
+		} else {
+		echo('Erro na creación da BD: ' . mysqli_error($conexionBD) . '<br/>');
+		}
+
+		//Desconexión
+		if (!@mysqli_close($conexionBD)) {
+			echo('Erro número ' . mysqli_errno($conexionBD) . ' ao pechar a conexión: ' . mysqli_error($conexionBD) . '.<br/>');
+		} else {
+			echo('Conexión pechada con éxito');
+		}
+	}
+?>
